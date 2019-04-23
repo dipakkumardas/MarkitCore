88f53f4df226af448792eb432f02d4d22e2b20e5
unknown <dipak@DIPAKDAS-PC.indusnettechnologies.com>
2019-04-23 15:31:40 +0530

    LoginTestSuite2

---
+++ Test Suites/Login.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>1af8cabb-99a2-4971-a310-31084038e95a</testSuiteGuid>
+   <testCaseLink>
+      <guid>039c4405-f140-4455-80a5-c80ae24ad72d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01-LoginApplication</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
